1bb9569340d4296f419eff761e2cb1a84481d8d1
Bartosz <prawo123@interia.pl>
2017-08-01 14:44:00 +0200

    added example data to verify propriety of markup

--- server/server.js
+++ server/server.js
@@ -4,11 +4,41 @@ const app = express();
 
 app.engine('handlebars', hbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars'); //ustawiamy silnik szablonow na handlebars
-app.use('/assets', express.static('public'));//sciezka do plikow statycznych
+app.use('/assets', express.static('public')); //sciezka do plikow statycznych
 
 app.get('/', (req, res) => {
     res.render('home', {
-        title: 'HOME'
+        main_news: {
+            title: 'VIRTUS.PRO WYGRYWA PGL MAJOR W KRAKOWIE!',
+            img: '../assets/img/news/vp_winning.jpg'
+        },
+        mid_news: {
+            first: {
+                title: 'LOL: MISTRZOSTWA ŚWIATA 2017, DZIEŃ 1',
+                img: '../assets/img/news/lol_championships.jpg'
+            },
+            second: {
+                title: 'LOL: MISTRZOSTWA ŚWIATA 2017, DZIEŃ 2',
+                img: '../assets/img/news/lol_championships.jpg'
+            }
+        },
+        small_news: {
+            first: {
+                title: 'VIRTUS.PRO TAKE DOWN FNATIC IN KRAKÓW',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam nemo unde placeat numquam mollitia enim eveniet, maiores consequatur dolore rem.',
+                img: '../assets/img/news/taz.jpg'
+            },
+            second: {
+                title: 'VIRTUS.PRO TAKE DOWN FNATIC IN KRAKÓW',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam nemo unde placeat numquam mollitia enim eveniet, maiores consequatur dolore rem.',
+                img: '../assets/img/news/taz.jpg'
+            },
+            third: {
+                title: 'VIRTUS.PRO TAKE DOWN FNATIC IN KRAKÓW',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam nemo unde placeat numquam mollitia enim eveniet, maiores consequatur dolore rem.',
+                img: '../assets/img/news/taz.jpg'
+            }
+        }
     });
 })
 

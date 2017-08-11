3d9c1d7a123078826f001a72cb1d961bbf00a4b6
Bartosz <prawo123@interia.pl>
2017-08-11 15:35:09 +0200

    error handling

--- server/server.js
+++ server/server.js
@@ -9,9 +9,11 @@ app.engine('handlebars', hbs({
 app.set('view engine', 'handlebars'); //ustawiamy silnik szablonow na handlebars
 app.use('/assets', express.static('public')); //sciezka do plikow statycznych
 
+
+//GETS
 app.get('/', (req, res) => {
     res.render('home', {
-        title:'Virtus.Pro wygrywa PGL Major w Krakowie',
+        title: 'Virtus.Pro wygrywa PGL Major w Krakowie',
         main_news: {
             title: 'Virtus.Pro wygrywa PGL Major w Krakowie!',
             img: '../assets/img/news/vp_winning.jpg'
@@ -50,6 +52,26 @@ app.get('/news/:category/:title', (req, res) => {
         text: '<p>Animi explicabo possimus, pariatur suscipit numquam quis sequi molestiae ratione atque eligendi, labore nemo dolorum delectus nam at culpa id doloremque aliquam ducimus. Voluptatum labore itaque aliquid rerum debitis blanditiis obcaecati odio ex unde vel provident, in fugiat, laudantium, amet nam. Obcaecati placeat vel eligendi, quos quod ratione repudiandae perspiciatis modi. Consequatur iste accusamus, odio, in, deserunt tempora facere placeat magnam aut exercitationem doloremque facilis iusto sequi pariatur beatae tempore earum necessitatibus porro, nihil.</p> <p>Assumenda pariatur illo voluptates nemo minus! Impedit natus unde corporis molestias qui laudantium veniam assumenda aut in adipisci, fuga, modi possimus quo obcaecati. Beatae quaerat exercitationem dolorum, nam repellendus quam sequi ipsa labore voluptatem enim aperiam modi porro iusto vel architecto quisquam saepe omnis, vitae sapiente nulla numquam officia et. Quidem, voluptas alias facere quas, dignissimos tempora tempore a doloribus et illum recusandae aliquid magnam dolore cupiditate voluptates minus nisi aperiam ut vel perferendis, ea magni assumenda nam. Ab magni cumque aspernatur libero, obcaecati cupiditate! Quia ratione beatae, eligendi sunt consectetur expedita repellat veritatis. Voluptatum ex ullam quas commodi, tempora officiis consequuntur adipisci veniam recusandae, doloremque voluptas asperiores at dignissimos esse, quo, necessitatibus impedit repudiandae! Dolores molestias officiis dolor eveniet quasi nam, quibusdam rerum id quo corporis tempore cumque excepturi cum enim hic reprehenderit. Ad ab minima cum quasi eius, cumque! Quia blanditiis nemo dolorem. Molestiae qui quia quibusdam iusto repellendus. Totam nesciunt modi earum doloribus ea ut quae debitis saepe, tenetur numquam, quo asperiores natus tempore provident consequuntur.</p><p> Repellat tempore tempora possimus sunt laborum fuga voluptatibus a molestiae temporibus laudantium repellendus, eos reiciendis eveniet, deserunt consequuntur inventore accusantium, explicabo vitae commodi laboriosam? Animi minus omnis officiis accusantium temporibus dolore eveniet ex nisi enim recusandae. <p>Provident fuga odit fugit natus rerum. Suscipit dicta error ex a neque, ab accusamus sint minima voluptates, laborum unde veritatis quisquam enim, tempora?</p>'
     });
 });
+
+app.get('/profil/register', (req, res) => {
+    res.render('register', {
+        test: 'test'
+    })
+})
+
+//POSTS
+app.post('/profil/register', (req, res) => {
+    res.send('WYSŁAŁEM FORMULARZ');
+})
+
+//ERROR HANDLING
+app.use((req, res) => {
+    res.status(404).render('404', {
+        title: 'ERROR 404',
+        desc: 'Coś poszło nie tak :(',
+        fun: 'Strona w budowie.'
+    })
+})
 
 app.listen(port, () => {
     console.log('Server running on localhost:3000');

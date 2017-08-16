321fb6b894e9cb71841954ae04c9497a15172560
Bartosz <prawo123@interia.pl>
2017-08-16 12:52:44 +0200

    author model added to the database

---
+++ models/author.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const authorSchema = mongoose.Schema({
+    nickname: {
+        type: String,
+        unique: true
+    },
+    rank: String,
+    avatar: String,
+    info: String
+});
+
+var Author = mongoose.model('Author', authorSchema);
+
+module.exports = {
+    Author
+};

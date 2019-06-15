b34fc9965debb2aeeafc96a59114bd9f729fa0c7
XaooB <prawo123@interia.pl>
2019-06-15 19:30:35 +0200

    single article update

--- models/comment.js
+++ models/comment.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose'),
-      {getDate} = require('./../public/js/getDate'),
-      moment = require('moment');
+const mongoose = require("mongoose"),
+  { getDate } = require("./../public/js/getDate"),
+  moment = require("moment");
 mongoose.Promise = global.Promise;
 
 const commentSchema = mongoose.Schema({
-    username: String,
-    body: String,
-    title: String,
-    category: String,
-    date: {
-        type: Date,
-        default: new Date
-    }
+  username: String,
+  body: String,
+  title: String,
+  category: String,
+  date: {
+    type: Date,
+    default: new Date()
+  }
 });
 
-var Comment = mongoose.model('Comment', commentSchema);
+var Comment = mongoose.model("Comment", commentSchema);
 
 module.exports = {
-    Comment
+  Comment
 };
